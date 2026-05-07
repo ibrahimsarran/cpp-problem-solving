a31047330d1965589f414bd4ff3182f272f898ff
ibrahimsarran <ibrahim.a.sarran@gmail.com>
2026-05-07 03:32:00 +0300

    edit problem 1 level1

---
+++ Level1/Problem #1 Print Name.cpp
@@ -0,0 +1,17 @@
+//Problem #1 Print Name
+#include <iostream>
+#include <string>
+
+using namespace std;
+void printName(string name)
+{
+    cout<<"\nYour name is: "<<name<<endl;
+}
+
+int main()
+{
+
+    printName("Ibrahim Sarran");
+
+    return 0;
+}
