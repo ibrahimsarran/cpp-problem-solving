87f7cc601dec2051ed15ac4fd4439d1be75c4df5
ibrahimsarran <ibrahim.a.sarran@gmail.com>
2026-07-09 00:20:53 +0300

    first two problems in Level3

---
+++ Level3/Problem #1- 3x3 Random Matrix.cpp
@@ -0,0 +1,53 @@
+// #1/3 - 3*3 Random Matrix 
+
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int RandomNumber(int from, int to)
+{
+	return rand() % (to - from + 1) + 1;
+}
+
+void GenerateTwoDimArray(int x[3][3],short Rows, short Cols)
+{
+	
+	for (int i = 0; i < Rows;i++)
+	{
+		for (int j = 0; j < Cols;j++)
+		{
+			x[i][j] = RandomNumber(1,100);
+		}
+	}
+}
+
+void PrintTwoDimArray(int x[3][3], short Rows, short Cols)
+{
+	cout << "\nThe Two Dim Array:\n";
+	for (int i = 0; i < Rows;i++)
+
+	{
+		for (int j = 0; j < Cols; j++)
+		{
+			cout << setw(3) << x[i][j] << "   ";
+		}
+		
+		cout << "\n";
+	}
+
+	cout << endl;
+}
+int main() 
+
+{
+	srand((unsigned)time(NULL));
+
+	int x[3][3];
+	GenerateTwoDimArray(x,3,3);
+	PrintTwoDimArray(x,3,3);
+
+
+	return 0;
+}
